28ea27dff494a8691308a35e03cca2d03e84e296
zhouhaobit <329998641@qq.com>
2020-04-03 22:17:11 +0800

    小程序开发学习

---
+++ util/http.js
@@ -0,0 +1,38 @@
+
+import {config} from '../config.js'
+class HTTP{
+  //statusCode
+  request(params){
+      if(!params.method){
+        params.method= 'GET'
+      }
+      wx.request({
+        url: config.api_base_url+params.url,
+        method:params.method,
+        data:params.data,
+
+        header:{
+          'content-type':'application/json',
+          'appkey':config.appkey,
+        },
+        success:(res)=>{
+          let code =res.statusCode.toString()
+          if(code.startsWith('2') ){
+              params.success(res.data)
+          }else{
+
+            wx.showToast({
+              title:'有个错误',
+              duration:2000
+            })
+
+          }
+        },
+        fail:(err)=>{
+
+        }
+      })
+  }
+}
+
+export {HTTP}
