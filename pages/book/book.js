7eff6e609433729dff66e3d46bff4e5f91b4a674
zhouhaobit <329998641@qq.com>
2020-04-08 18:23:55 +0800

    小程序学习代码

---
+++ pages/book/book.js
@@ -0,0 +1,86 @@
+// pages/book/book.js
+import {
+  BookModel
+} from '../../models/book.js'
+
+let bookModel = new BookModel()
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    books:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const promise = bookModel.getHotBooks()
+    promise.then((res)=>{
+     // console.log(res)
+     this.setData({
+       books:res
+     })
+    }
+    )
+
+    /*避免回调地狱写法
+    bookModel.getHotBooks().then(res=>{
+      console.log(res)
+      return bookModel.getMyBookCount()
+    }).then(res=>{
+      console.log(res)
+    })
+    */
+    
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
